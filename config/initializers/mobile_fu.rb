9971305b5f95e42c96338ef9491b86abc4021aa4
Hasan Diwan <hd1@jsc.d8u.us>
2019-09-24 16:08:03 -0700

    get rid of deprecation warning for alias_method_chain

--- config/initializers/mobile_fu.rb
+++ config/initializers/mobile_fu.rb
@@ -4,4 +4,8 @@ require Rails.root + 'lib/mobile-fu/lib/mobile_fu'
 
 ActionView::Base.send(:include, MobileFuHelper)
 ActionView::Base.send(:include, MobilizedStyles)
-ActionView::Base.send(:alias_method_chain, :stylesheet_link_tag, :mobilization)
+module MobilizationMethod
+  def stylesheet_link_tag
+    return mobilization(:params)
+  end
+end
